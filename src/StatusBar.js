fdaa4d29e59397b0c310284bee247c3c7d73bf81
antonina_n <antonina.nevstedt@consid.se>
2020-07-30 23:36:08 +0200

    Another StatusBar fix

--- src/StatusBar.js
+++ src/StatusBar.js
@@ -1,22 +1,25 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-import formatTime from "./utils.js"
+import formatTime from "./utils.js";
 import "./StatusBar.css";
+import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 
 function StatusBar({ timeMs, score, onRestart, showLeaderboard }) {
   return (
     <div>
       <div className="status-container">
         <div className="text-group">
-          <p className="text">{score}</p>
-          <p className="text">
-            Time: {formatTime(timeMs)}
-          </p>
+          <p className="text">Time: {formatTime(timeMs)}</p>
+          <p className="text">{score}&nbsp;</p>
         </div>
         <Button variant="light" className="rightButton" onClick={onRestart}>
           Restart
         </Button>
-        <Button variant="light" className="rightButton" onClick={showLeaderboard}>
+        <Button
+          variant="light"
+          className="rightButton"
+          onClick={showLeaderboard}
+        >
           Leaderboard
         </Button>
       </div>
